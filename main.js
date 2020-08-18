e992cf7221e1b893e5710e3b46283ac96f4b2605
ryan_oh <shcoshcoshco@gmail.com>
2020-08-18 21:30:13 +0900

    JS: Timer

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const gameBtn = document.querySelector('.game__btn');
+const gameTimer = document.querySelector('.game__timer');
 const gameField = document.querySelector('.game__field');
 
 let isPlay = false;
+let timer = undefined;
+let restTime = 10;
 
 gameBtn.addEventListener('click', () => {
   if (!isPlay) {
@@ -15,6 +18,7 @@ gameBtn.addEventListener('click', () => {
 
 function initGame() {
   isPlay = false;
+  restTime = 5;
   makeObjects('carrot', 5, './img/carrot.png');
   makeObjects('bug', 5, './img/bug.png');
   hideObjects();
@@ -24,16 +28,19 @@ function startGame() {
   isPlay = true;
   showPauseBtn();
   showObjects();
+  startTimer();
 }
 
 function pauseGame() {
   isPlay = false;
   showPlayBtn();
   hideObjects();
+  stopTimer();
 }
 
 function finishGame() {
   isPlay = false;
+  stopTimer();
 }
 
 function showPauseBtn() {
@@ -46,6 +53,12 @@ function showPlayBtn() {
   const btn = gameBtn.querySelector('.fas');
   btn.classList.remove('fa-pause');
   btn.classList.add('fa-play');
+}
+
+function showRestTime(time) {
+  const min = Math.floor(time / 60);
+  const sec = time % 60;
+  gameTimer.textContent = `${('0' + min).slice(-2)}:${('0' + sec).slice(-2)}`;
 }
 
 function makeObjects(type, number, imgPath) {
@@ -79,6 +92,20 @@ function hideObjects() {
   bugs.forEach(bug => {
     bug.style.visibility = 'hidden';
   });
+}
+
+function startTimer() {
+  timer = setInterval(() => {
+    --restTime;
+    showRestTime(restTime);
+    if (restTime <= 0) {
+      finishGame();
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timer);
 }
 
 initGame();
